3d11f2b3b2be698ead3581434741dbd5084b8c5a
rohantotherescue <rohantotherescue@gmail.com>
2024-05-20 15:49:25 +0530

    home component

---
+++ src/Cities.tsx
@@ -0,0 +1,22 @@
+export const cities = [
+    {
+      name: 'New Delhi',
+      image: require('../assets/images/delhi.jpg'),
+    },
+    {
+      name: 'New York',
+      image: require('../assets/images/newyork.png'),
+    },
+    {
+      name: 'London',
+      image: require('../assets/images/london.png'),
+    },
+    {
+      name: 'San Francisco',
+      image: require('../assets/images/sanf.png'),
+    },
+    {
+      name: 'New Jersey',
+      image: require('../assets/images/njersey.png'),
+    },
+  ];
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,54 @@
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ImageBackground, StyleSheet, TouchableOpacity, TextInput, FlatList } from 'react-native'
+import React, {useState} from 'react'
 import { deviceHeight, deviceWidth } from './Dimensions'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { cities } from './Cities';
+
 
 const Home= ()=>  {
+    const [city, setCity] = useState('');
   return (
     <View>
       <ImageBackground source={require('../assets/images/background.png')} style= {styles.bgImg} imageStyle= {styles.bgImg}>
+        <View style= {styles.sneed}>
+            <View style=  {styles.head}>
+            <Icon name="menu" size={46} color="white" />
+            {/* <Image
+            source={require('../assets/images/user.jpg')}
+            style={{height: 46, width: 46, borderRadius: 50}}
+          /> */}
+            </View>
+            <View style={{paddingHorizontal: 20, marginTop: 100}}>
+            <Text style={{fontSize: 40, color: 'white'}}>Hello Rohan.</Text>
+            {/* <Text style={{color: 'white', fontSize: 22, fontWeight: 'bold'}}>
+                Search your city..
+            </Text> */}
+            <View
+            style={styles.searchBar}>
+            <TextInput
+            value={city}
+            onChangeText={val => setCity(val)}
+              placeholder="Search City"
+              placeholderTextColor="white"
+              style={styles.searchInput}
+            />
+            <TouchableOpacity onPress={() =>{}}>
+              <Icon name="search" size={22} color="white" />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.savedLocations}>
+            Saved Locations
+          </Text>
+            <FlatList horizontal data={cities} renderItem={() => (
+                <View>
+                    </View>
+  )}/>
+            </View>
+            <View>
 
+            </View>
+
+        </View>
       </ImageBackground>
     </View>
   )
@@ -19,7 +61,31 @@ const styles= StyleSheet.create(
             width: deviceWidth,
             opacity: 0.6,
             backgroundColor: 'black'
-        }
+        },
+        sneed: {
+            position: 'absolute',
+            paddingVertical: 20,
+            paddingHorizontal: 10,
+          },
+          head:{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: deviceWidth - 20,
+          },
+          searchBar: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            borderRadius: 50,
+            borderWidth: 1,
+            borderColor: 'white',
+            marginTop: 16,
+            paddingHorizontal: 10,
+          },
+          searchInput : {paddingHorizontal: 10, color: 'white', fontSize: 16},
+          savedLocations: {color: 'white', fontSize: 25, paddingHorizontal: 10, marginTop: 220, marginBottom: 20}
+          
     }
 )
 

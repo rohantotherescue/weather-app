4b2b3a75d74eeb17d7e6bb023610117a6965491e
rohantotherescue <rohantotherescue@gmail.com>
2024-05-20 13:26:38 +0530

    added background image

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
 import React from 'react'
+import { deviceHeight, deviceWidth } from './Dimensions'
 
-export default function Home() {
+const Home= ()=>  {
   return (
     <View>
-      <Text>Home</Text>
+      <ImageBackground source={require('../assets/images/background.png')} style= {styles.bgImg} imageStyle= {styles.bgImg}>
+
+      </ImageBackground>
     </View>
   )
 }
+
+const styles= StyleSheet.create(
+    {
+        bgImg: {
+            height: deviceHeight,
+            width: deviceWidth,
+            opacity: 0.6,
+            backgroundColor: 'black'
+        }
+    }
+)
+
+export default Home;
